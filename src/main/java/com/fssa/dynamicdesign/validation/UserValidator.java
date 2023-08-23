2523900970402d97bbc6ade533458ffcd6f0366f
Naveena-007 <118426442+Naveena-007@users.noreply.github.com>
2023-08-23 14:29:38 +0530

    user validator

--- src/main/java/com/fssa/dynamicdesign/validation/UserValidator.java
+++ src/main/java/com/fssa/dynamicdesign/validation/UserValidator.java
@@ -1,6 +1,5 @@
 package com.fssa.dynamicdesign.validation;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.fssa.dynamicdesign.model.User;
@@ -8,118 +7,68 @@ import com.fssa.dynamicdesign.validation.exception.InvalidUserException;
 
 public class UserValidator {
 
-	public static boolean validateUser(User user) throws InvalidUserException {
-		if (user != null && validateUserID(user.getUserId())&& validateName(user.getUsername()) && validatePassword(user.getPassword())
-				&& validateEmail(user.getEmail()) && validatePhoneNumber(user.getPhonenumber())) {
-			return true;
-		} else {
-			throw new InvalidUserException("User details not valid");
-		}
-	}
+    public static boolean validateUser(User user) throws InvalidUserException {
+        if (user == null || !isValidUser(user)) {
+            throw new InvalidUserException("User details are not valid");
+        }
+        return true;
+    }
 
-	
-	public static boolean validateUserID(int userID) throws InvalidUserException {
-		if (userID < 0) {
-			throw new InvalidUserException("User ID is invalid: Negative value.");
-		}
+    private static boolean isValidUser(User user) {
+        try {
+            return validateUserID(user.getUserId())
+                    && validateName(user.getUsername())
+                    && validatePassword(user.getPassword())
+                    && validateEmail(user.getEmail())
+                    && validatePhoneNumber(user.getPhonenumber());
+        } catch (InvalidUserException e) {
+            // Handle the exception or re-throw it as necessary
+            e.printStackTrace(); // Just printing the stack trace as an example
+            return false;
+        }
+    }
 
-		String regexUserID = "^\\d+$";
-		boolean match = Pattern.matches(regexUserID, Integer.toString(userID));
+    public static boolean validateUserID(int userID) throws InvalidUserException {
+        if (userID < 0) {
+            throw new InvalidUserException("User ID is invalid: Negative value");
+        }
+        return Pattern.matches("^\\d+$", Integer.toString(userID));
+    }
 
-		if (match) {
-			System.out.println("User ID is valid.");
-		} else {
-			throw new InvalidUserException("User ID is invalid: Only non-negative numbers are allowed.");
-		}
+    public static boolean validateName(String name) throws InvalidUserException {
+        if (name == null || !Pattern.matches("^[A-Za-z]\\w{2,29}$", name)) {
+            throw new InvalidUserException("User name is not valid");
+        }
+        return true;
+    }
 
-		return match;
-	}
+    public static boolean validatePassword(String password) throws InvalidUserException {
+        if (password == null || !Pattern.matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=])(?=.*[^\\s]).{8,}$", password)) {
+            throw new InvalidUserException("User password is not valid");
+        }
+        return true;
+    }
 
-	
-	
-	
-	public static boolean validateName(String name) throws InvalidUserException {
-		if (name == null) {
-			throw new InvalidUserException("User name is null");
-		}
+    public static boolean validateEmail(String email) throws InvalidUserException {
+        if (email == null || !Pattern.matches("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", email)) {
+            throw new InvalidUserException("Email address is not valid");
+        }
+        return true;
+    }
 
-		String regex = "^[A-Za-z]\\w{2,29}$";
-		Pattern p = Pattern.compile(regex);
-		Matcher m = p.matcher(name);
-		boolean match = m.matches();
+    public static boolean validatePhoneNumber(String phoneNumber) throws InvalidUserException {
+        if (phoneNumber == null || !Pattern.matches("^(\\+?91|91)?[6-9]\\d{9}$", phoneNumber)) {
+            throw new InvalidUserException("Phone number is not valid");
+        }
+        return true;
+    }
 
-		if (match) {
-			System.out.println("The User name is valid.");
-		} else {
-			throw new InvalidUserException("The User name is not valid");
-		}
-
-		return match;
-	}
-	
-	
-
-	public static boolean validatePassword(String password) throws InvalidUserException {
-	    boolean isMatch = true;
-	    if (password == null) {
-	        return false;
-	    }
-	    String patternString = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=])(?=.*[^\\s]).{8,}$";
-	    Pattern pattern = Pattern.compile(patternString);
-	    isMatch = pattern.matcher(password).matches();
-
-	    if (isMatch) {
-	        System.out.println("Valid password.");
-	    } else {
-	        throw new InvalidUserException("User password is InValid");
-	    }
-
-	    return isMatch;
-	}
-
-
-	
-	public static boolean validateEmail(String email) throws InvalidUserException {
-		if (email == null) {
-			throw new InvalidUserException("Email address is null");
-		}
-
-		String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
-		boolean isMatch = Pattern.matches(regex, email);
-
-		if (isMatch) {
-			System.out.println("The email address is: Valid");
-		} else {
-			throw new InvalidUserException("The email address is: Invalid");
-		}
-
-		return isMatch;
-	}
-
-	public static boolean validatePhoneNumber(String phoneNumber) throws InvalidUserException {
-		if (phoneNumber == null)
-			return false;
-
-		String regex = "^(\\+?91|91)?[6-9]\\d{9}$";
-		Pattern pattern = Pattern.compile(regex);
-		Matcher matcher = pattern.matcher(phoneNumber);
-		boolean isMatch = matcher.matches();
-
-		if (isMatch) {
-			System.out.println("The phone number is: Valid");
-		} else {
-			throw new InvalidUserException("The phone number is: Invalid");
-		}
-
-		return isMatch;
-	}
-
-	public static boolean validateDeleteUser(User user) throws InvalidUserException {
-		if (user != null && user.getUserId() > 0 && validateEmail(user.getEmail())) {
-			System.out.println("User deletion details are valid.");
-			return true;
-		} else {
-			throw new InvalidUserException("Invalid user details for deletion");
-		}
-	}
+    public static boolean validateDeleteUser(User user) throws InvalidUserException {
+        if (user != null && user.getUserId() > 0 && validateEmail(user.getEmail())) {
+            System.out.println("User deletion details are valid.");
+            return true;
+        } else {
+            throw new InvalidUserException("Invalid user details for deletion");
+        }
+    }
 }
